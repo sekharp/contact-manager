487ad1becbd75f820b63da64bd8d014e4e1884fc
Sekhar Paladugu <sekharp@gmail.com>
2015-10-31 16:13:36 -0600

    tests green!!!!! success

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -18,7 +18,6 @@ RSpec.describe PhoneNumber, type: :model do
   end
 
   it 'is associated with a person' do
-    expect(phone_number).to respond_to(:person
-    )
+    expect(phone_number).to respond_to(:person)
   end
 end
