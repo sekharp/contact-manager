2a9058a54cab42aa9efc6ef843b8358bcbdf6754
Sekhar Paladugu <sekharp@gmail.com>
2015-10-31 17:16:48 -0600

    finished i2

--- spec/controllers/phone_numbers_controller_spec.rb
+++ spec/controllers/phone_numbers_controller_spec.rb
@@ -148,6 +148,9 @@ RSpec.describe PhoneNumbersController, type: :controller do
   end
 
   describe "DELETE #destroy" do
+    let(:jim) { Person.create(first_name: 'Jim', last_name: 'Jones') }
+    let(:valid_attributes) { {number: '333-4444', person_id: jim.id} }
+
     it "destroys the requested phone_number" do
       phone_number = PhoneNumber.create! valid_attributes
       expect {
@@ -158,7 +161,7 @@ RSpec.describe PhoneNumbersController, type: :controller do
     it "redirects to the phone_numbers list" do
       phone_number = PhoneNumber.create! valid_attributes
       delete :destroy, {:id => phone_number.to_param}, valid_session
-      expect(response).to redirect_to(phone_numbers_url)
+      expect(response).to redirect_to(jim)
     end
   end
 
--- spec/features/person_view_spec.rb
+++ spec/features/person_view_spec.rb
@@ -45,4 +45,19 @@ describe 'the person view', type: :feature do
     expect(page).to have_content('555-9191')
     expect(page).to_not have_content(old_number)
   end
+
+  it 'has links to delete phone numbers' do
+    person.phone_numbers.each do |phone|
+      expect(page).to have_link('delete', href: phone_number_path(phone))
+    end
+  end
+
+  it 'deletes a phone number' do
+    phone = person.phone_numbers.first
+    old_number = phone.number
+
+    first(:link, 'delete').click
+    expect(current_path).to eq(person_path(person))
+    expect(page).to_not have_content(old_number)
+  end
 end
